7be5a032616e2e710285cb0d5ac68805b23e5625
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/HideInEditModePlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/HideInEditModePlugin.pm
+++ lib/TWiki/Plugins/HideInEditModePlugin.pm
@@ -11,7 +11,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -20,29 +20,31 @@ package TWiki::Plugins::HideInEditModePlugin;
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $pluginName
-        $debug $exampleCfgVar
-    );
+  $web $topic $user $installWeb $VERSION $pluginName
+  $debug $exampleCfgVar
+);
 
-
-$VERSION = '$Rev$';
-$pluginName = 'HideInEditModePlugin';  # Name of this Plugin
+$VERSION    = '$Rev$';
+$pluginName = 'HideInEditModePlugin';    # Name of this Plugin
 
 # =========================
 sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
+    $debug = TWiki::Func::getPreferencesFlag("\U$pluginName\E_DEBUG");
 
     # Plugin correctly initialized
-    TWiki::Func::writeDebug( "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    TWiki::Func::writeDebug(
+        "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+      if $debug;
     return 1;
 }
 
@@ -50,7 +52,8 @@ sub initPlugin {
 sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    TWiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::commonTagsHandler( $_[2].$_[1] )")
+      if $debug;
 
     $_[0] =~ s/%STARTHIDDEN%(.*?)%ENDHIDDEN%/$1/sg;
 }
@@ -59,33 +62,41 @@ sub commonTagsHandler {
 sub beforeEditHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
     my $topic = $_[1];
-    my $web = $_[2];
+    my $web   = $_[2];
 
-    TWiki::Func::writeDebug( "- ${pluginName}::beforeEditHandler( $web.$topic )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::beforeEditHandler( $web.$topic )")
+      if $debug;
 
-    # This handler is called by the edit script just before presenting the edit text
-    # in the edit box. Use it to process the text before editing.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
+# This handler is called by the edit script just before presenting the edit text
+# in the edit box. Use it to process the text before editing.
+# New hook in TWiki::Plugins $VERSION = '1.010'
 
-    my $wikiUser = TWiki::Func::getWikiUserName();	
-    if (!TWiki::Func::checkAccessPermission("HIDDEN", $wikiUser, $_[0], $topic, $web)) {
-	# Shouldn't see hidden portion in edit mode
-	if ($_[0] =~ s/%STARTHIDDEN%(.*?)%ENDHIDDEN%//s) {
-	    saveHiddenPortion($topic, $web, $1);
-	}
+    my $wikiUser = TWiki::Func::getWikiUserName();
+    if (
+        !TWiki::Func::checkAccessPermission(
+            "HIDDEN", $wikiUser, $_[0], $topic, $web
+        )
+      )
+    {
+
+        # Shouldn't see hidden portion in edit mode
+        if ( $_[0] =~ s/%STARTHIDDEN%(.*?)%ENDHIDDEN%//s ) {
+            saveHiddenPortion( $topic, $web, $1 );
+        }
     }
 }
 
 sub saveHiddenPortion {
-    my ($topic, $web, $hide) = @_;
+    my ( $topic, $web, $hide ) = @_;
 
-    my $wikiUser = TWiki::Func::getWikiUserName();	
-    $hide =~ s/&amp\;/&/go;    
+    my $wikiUser = TWiki::Func::getWikiUserName();
+    $hide =~ s/&amp\;/&/go;
     $hide =~ s/&lt\;/</go;
     $hide =~ s/&gt\;/>/go;
     $filename = $wikiUser . '.' . time();
-    TWiki::Func::setSessionValue("hideineditmode", $filename);
-    TWiki::Contrib::MoreFuncContrib::saveWorkFile("HideInEditModePlugin", "$filename", $hide);
+    TWiki::Func::setSessionValue( "hideineditmode", $filename );
+    TWiki::Contrib::MoreFuncContrib::saveWorkFile( "HideInEditModePlugin",
+        "$filename", $hide );
 }
 
 # =========================
@@ -93,20 +104,24 @@ sub beforeSaveHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
     my $topic = $_[1];
-    my $web = $_[2];
+    my $web   = $_[2];
 
-    TWiki::Func::writeDebug( "- ${pluginName}::afterEditHandler( $web.$topic )" ) if $debug;
+    TWiki::Func::writeDebug("- ${pluginName}::afterEditHandler( $web.$topic )")
+      if $debug;
 
-    # This handler is called by the preview script just before presenting the text.
-    # New hook in TWiki::Plugins $VERSION = '1.010'
-    if ($filename =	TWiki::Func::getSessionValue("hideineditmode")) {
-	TWiki::Func::clearSessionValue("hideineditmode");
-	$storage = TWiki::Contrib::MoreFuncContrib::readWorkFile("HideInEditModePlugin", "$filename");
-	TWiki::Contrib::MoreFuncContrib::deleteWorkFile("HideInEditModePlugin", "$filename");
+ # This handler is called by the preview script just before presenting the text.
+ # New hook in TWiki::Plugins $VERSION = '1.010'
+    if ( $filename = TWiki::Func::getSessionValue("hideineditmode") ) {
+        TWiki::Func::clearSessionValue("hideineditmode");
+        $storage =
+          TWiki::Contrib::MoreFuncContrib::readWorkFile( "HideInEditModePlugin",
+            "$filename" );
+        TWiki::Contrib::MoreFuncContrib::deleteWorkFile( "HideInEditModePlugin",
+            "$filename" );
 
-	if ($storage) {
-	    $_[0] = "$storage\n$_[0]";
-	}
+        if ($storage) {
+            $_[0] = "$storage\n$_[0]";
+        }
     }
 }
 
--- test/unit/HideInEditModePlugin/HideInEditModePluginSuite.pm
+++ test/unit/HideInEditModePlugin/HideInEditModePluginSuite.pm
@@ -3,8 +3,8 @@ package HideInEditModePluginSuite;
 use Unit::TestSuite;
 our @ISA = qw( Unit::TestSuite );
 
-sub name { 'HideInEditModePluginSuite' };
+sub name { 'HideInEditModePluginSuite' }
 
-sub include_tests { qw(HideInEditModePluginTests) };
+sub include_tests { qw(HideInEditModePluginTests) }
 
 1;
